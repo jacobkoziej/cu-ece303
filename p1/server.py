3218bcac641688638f422e6786d7c794c0acc357
Jacob Koziej <jacobkoziej@gmail.com>
2024-02-07 00:31:38 -0500

    Create socket

--- p1/server.py
+++ p1/server.py
@@ -4,9 +4,39 @@
 # server.py -- simple HTTP server
 # Copyright (C) 2024  Jacob Koziej <jacobkoziej@gmail.com>
 
+import argparse
+import socket
+
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        prog='server',
+        description='simple HTTP server',
+    )
+
+    parser.add_argument(
+        "-a",
+        "--address",
+        required=True,
+        type=str,
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        required=True,
+        type=int,
+    )
+
+    args = parser.parse_args()
+
+    sock = socket.socket(
+        family=socket.AF_INET,
+        type=socket.SOCK_STREAM,
+    )
+    sock.bind((args.address, args.port))
+    sock.listen()
+
+    sock.close()
 
 
 if __name__ == '__main__':
